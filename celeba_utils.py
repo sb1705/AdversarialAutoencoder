a16202739f28e52566d6e2ded5fafb69b80823d4
Sara Brolli <brolli.sr@gmail.com>
2017-09-04 18:37:29 +0200

    Immagini 64x64

--- celeba_utils.py
+++ celeba_utils.py
@@ -10,11 +10,18 @@ def celeba_process(x):
 #returns n images for training and n/5 images for testing
 #x_train, x_test: uint8 array of RGB image data with shape (num_samples, 3, 32, 32).
 
-def celeba_data(imageDirectory, n):
+def celeba_data(imageDirectory, n, size):
+    print("la size e ")
+    print(size)
     xtrain=io.imread(imageDirectory+os.listdir(imageDirectory)[0])
-    if xtrain.shape[0]!=32:
-        bulkResize(imageDirectory, 32)
-        imageDirectory=imageDirectory+ "/resized" + str(32) + "/"
+    if xtrain.shape[0]!=size:
+        print("inizio il resize")
+        if(os.path.isdir(imageDirectory+ "/resized" + str(size) + "/")):
+            print("il resize e gia stato fatto")
+        else:
+            print("uff devo fare tutto io")
+            bulkResize(imageDirectory, size)
+        imageDirectory=imageDirectory+ "/resized" + str(size) + "/"
         xtrain=io.imread(imageDirectory+os.listdir(imageDirectory)[0])
     xtrain=np.expand_dims(xtrain, axis=0)
     for file in os.listdir(imageDirectory)[1:n]:
